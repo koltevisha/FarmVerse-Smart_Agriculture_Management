6ea47fd35492bd44cfd838738e5db9244d84fe0c
FarmVerse User <user@farmverse.local>
2026-08-17 18:50:36 +0530

    update the webconfig for the production

--- FarmVerse/farmVerseApplication/src/main/java/edu/infosys/farmVerseApplication/config/SystemConfig.java
+++ FarmVerse/farmVerseApplication/src/main/java/edu/infosys/farmVerseApplication/config/SystemConfig.java
@@ -1,4 +1,5 @@
 package edu.infosys.farmVerseApplication.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,37 +8,43 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
- 
+
 @Configuration
 @EnableMethodSecurity
- 
-
 public class SystemConfig {
-	@Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-	  return configuration.getAuthenticationManager();
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
-@Bean
-SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-      .cors(Customizer.withDefaults())
-      .csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/farmverse/login/**").permitAll()
-            .requestMatchers("/farmverse/logout").permitAll()
-            .requestMatchers("/farmverse/**").permitAll()
-            .anyRequest().authenticated()
-      )
-      .logout(logout -> logout
-            .logoutUrl("/fincore/logout")
-            .invalidateHttpSession(true)
-            .deleteCookies("JSESSIONID")
-            .logoutSuccessHandler((request, response, authentication) -> {
-                response.setStatus(200);
-                response.getWriter().write("Logout success");
-            })
-      );
-    return http.build();
-}
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(Customizer.withDefaults())
+
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/farmverse/login/**").permitAll()
+                        .requestMatchers("/farmverse/logout").permitAll()
+                        .requestMatchers("/farmverse/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .logout(logout -> logout
+                        .logoutUrl("/farmverse/logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(200);
+                            response.getWriter().write("Logout success");
+                        })
+                );
+
+        return http.build();
+    }
 }
