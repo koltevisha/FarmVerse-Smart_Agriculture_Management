0dfbb3e61a372250e0d3302dad19bce281267d0f
Koltevishal45 <koltevishal45@gmail.com>
2026-08-21 23:20:15 +0530

    Delete FarmVerse directory

--- FarmVerse/farmVerseApplication/src/main/java/edu/infosys/farmVerseApplication/bean/CropExpense.java
+++
@@ -1,5 +0,0 @@
-package edu.infosys.farmVerseApplication.bean;
-
-public class CropExpense {
-
-}
--- FarmVerse/farmVerseApplication/src/main/java/edu/infosys/farmVerseApplication/dao/FarmUserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.infosys.farmVerseApplication.dao;
-import edu.infosys.farmVerseApplication.bean.FarmUser;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface FarmUserRepository extends JpaRepository<FarmUser,String> {
-}
